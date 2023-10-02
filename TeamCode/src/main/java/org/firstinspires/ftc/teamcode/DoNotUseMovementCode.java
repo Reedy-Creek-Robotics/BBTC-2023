2af91ff11e040652846cbaf6d21b8d409498536c
FTC-BBTC <ftcbbtc@gmail.com>
2023-10-02 17:12:35 -0400

    Testing bbtc-ftc account for push

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DoNotUseMovementCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DoNotUseMovementCode.java
@@ -13,7 +13,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@Autonomous(name = "!!!!DO NOT USE!!!!")
+@Autonomous(name = "!!!DO NOT USE!!!")
 public class DoNotUseMovementCode extends LinearOpMode {
 
     private DcMotor frontLeft;
