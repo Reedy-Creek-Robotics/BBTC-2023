f6c9436c9e365c87aa161aa79f4ec177ce801443
Cocoiscool-531 <119710842+Cocoiscool-531@users.noreply.github.com>
2023-11-20 22:26:17 -0500

    removed unneeded

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotAutoDriveToAprilTagOmni.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotAutoDriveToAprilTagOmni.java
@@ -49,24 +49,21 @@ import java.util.concurrent.TimeUnit;
 public class RobotAutoDriveToAprilTagOmni extends LinearOpMode
 {
     // Adjust these numbers to suit your robot.
-    final double DESIRED_DISTANCE = 5.0; //  this is how close the camera should get to the target (inches)
+    final double desiredDistance = 5.0;  //  this is how close the camera should get to the target (inches)
 
-    //  Set the GAIN constants to control the relationship between the measured position error, and how much power is
-    //  applied to the drive motors to correct the error.
-    //  Drive = Error * Gain    Make these values smaller for smoother control, or larger for a more aggressive response.
-    final double SPEED_GAIN  =  0.02  ;   //  Forward Speed Control "Gain". eg: Ramp up to 50% power at a 25 inch error.   (0.50 / 25.0)
-    final double STRAFE_GAIN =  0.015 ;   //  Strafe Speed Control "Gain".  eg: Ramp up to 25% power at a 25 degree Yaw error.   (0.25 / 25.0)
-    final double TURN_GAIN   =  0.01  ;   //  Turn Control "Gain".  eg: Ramp up to 25% power at a 25 degree error. (0.25 / 25.0)
+    final double forwardGain  =  0.02  ;   //  Forward Speed Control "Gain". eg: Ramp up to 50% power at a 25 inch error.   (0.50 / 25.0)
+    final double strafeGain =  0.015 ;    //  Strafe Speed Control "Gain".  eg: Ramp up to 25% power at a 25 degree Yaw error.   (0.25 / 25.0)
+    final double turnGain   =  0.01  ;   //  Turn Control "Gain".  eg: Ramp up to 25% power at a 25 degree error. (0.25 / 25.0)
 
-    final double MAX_AUTO_SPEED = 0.5;   //  Clip the approach speed to this max value (adjust for your robot)
-    final double MAX_AUTO_STRAFE= 0.5;   //  Clip the approach speed to this max value (adjust for your robot)
-    final double MAX_AUTO_TURN  = 0.3;   //  Clip the turn speed to this max value (adjust for your robot)
+    final double maxAutoForwardSpeed = 0.5;   //  Clip the approach speed to this max value (adjust for your robot)
+    final double maxAutoStrafeSpeed= 0.5;    //  Clip the approach speed to this max value (adjust for your robot)
+    final double maxAutoTurnSpeed  = 0.3;   //  Clip the turnPower speed to this max value (adjust for your robot)
 
-    private static final boolean USE_WEBCAM = true;  // Set true to use a webcam, or false for a phone camera
-    private static final int DESIRED_TAG_ID = -1;    // Choose the tag you want to approach or set to -1 for ANY tag.
-    private VisionPortal visionPortal;               // Used to manage the video source.
+    private static final boolean USE_WEBCAM = true;     // Set true to use a webcam, or false for a phone camera
+    private static final int desiredTagID = -1;        // Choose the tag you want to approach or set to -1 for ANY tag.
+    private VisionPortal visionPortal;                // Used to manage the video source.
     private AprilTagProcessor aprilTag;              // Used for managing the AprilTag detection process.
-    private AprilTagDetection detectedTag = null;     // Used to hold the data for a detected AprilTag
+    private AprilTagDetection detectedTag = null;   // Used to hold the data for a detected AprilTag
 
 DcMotor driveFrontLeft;
 DcMotor driveFrontRight;
@@ -76,16 +73,15 @@ DcMotor driveBackRight;
     @Override public void runOpMode()
     {
         boolean targetFound = false;    // Set to true when an AprilTag target is detected
-        double drive = 0.3;        // Desired forward power/speed (-1 to +1)
-        double strafe = 0.3;        // Desired strafe power/speed (-1 to +1)
-        double turn = 0.3;        // Desired turning power/speed (-1 to +1)
+        double forwardPower = 0.3;     // Desired forward power/speed (-1 to +1)
+        double strafePower = 0.3;     // Desired strafePower power/speed (-1 to +1)
+        double turnPower = 0.3;      // Desired turning power/speed (-1 to +1)
 
         driveFrontLeft  = hardwareMap.get(DcMotor.class, "driveFrontLeft");
         driveFrontRight = hardwareMap.get(DcMotor.class, "driveFrontRight");
         driveBackLeft = hardwareMap.get(DcMotor.class, "driveBackLeft");
         driveBackRight = hardwareMap.get(DcMotor.class, "driveBackRight");
 
-        // Initialize the April tag Detection process
         initAprilTag();
 
         driveFrontLeft.setDirection(DcMotor.Direction.REVERSE);
@@ -93,12 +89,10 @@ DcMotor driveBackRight;
         driveFrontRight.setDirection(DcMotor.Direction.FORWARD);
         driveBackRight.setDirection(DcMotor.Direction.FORWARD);
 
-        if (USE_WEBCAM)
-            setManualExposure(6, 0);  // Use low exposure time to reduce motion blur
+        setManualExposure(6, 200);
 
         // Wait for driver to press start
-        telemetry.addData("Camera preview on/off", "3 dots, Camera Stream");
-        telemetry.addData(">", "Touch Play to start OpMode");
+        telemetry.addLine("> Touch Play to start OpMode");
         telemetry.update();
         waitForStart();
 
@@ -111,65 +105,47 @@ DcMotor driveBackRight;
             List<AprilTagDetection> currentDetections = aprilTag.getDetections();
             for (AprilTagDetection detection : currentDetections) {
                 if ((detection.metadata != null) &&
-                    ((DESIRED_TAG_ID < 0) || (detection.id == DESIRED_TAG_ID))  ){
+                    ((desiredTagID < 0) || (detection.id == desiredTagID))  ){
                     targetFound = true;
                     detectedTag = detection;
                     break;  // don't look any further.
                 } else {
-                    telemetry.addData("Unknown Target", "Tag ID %d is not in TagLibrary\n", detection.id);
+                    telemetry.addData("Unknown Target", "Tag ID %d is not desired tag\n", detection.id);
                 }
             }
 
-            // Tell the driver what we see, and what to do.
             if (targetFound) {
-                telemetry.addData(">","HOLD Left-Bumper to Drive to Target\n");
-                telemetry.addData("Target", "ID %d (%s)", detectedTag.id, detectedTag.metadata.name);
-                telemetry.addData("Range",  "%5.1f inches", detectedTag.ftcPose.range);
-                telemetry.addData("Bearing","%3.0f degrees", detectedTag.ftcPose.bearing);
-                telemetry.addData("Yaw","%3.0f degrees", detectedTag.ftcPose.yaw);
-            } else {
-                telemetry.addData(">","Drive using joysticks to find valid target\n");
+                telemetry.addData(">","HOLD Left-Bumper to Drive to Target");
             }
 
-            // If Left Bumper is being pressed, AND we have found the desired target, Drive to target Automatically .
             if (gamepad1.left_bumper && targetFound) {
-
                 // Determine heading, range and Yaw (tag image rotation) error so we can use them to control the robot automatically.
-                double  rangeError      = (detectedTag.ftcPose.range - DESIRED_DISTANCE);
-                double  headingError    = detectedTag.ftcPose.bearing;
-                double  yawError        = detectedTag.ftcPose.yaw;
+                double  rangeError   = (detectedTag.ftcPose.range - desiredDistance);
+                double  headingError = detectedTag.ftcPose.bearing;
+                double  yawError     = detectedTag.ftcPose.yaw;
 
-                // Use the speed and turn "gains" to calculate how we want the robot to move.
-                drive  = Range.clip(rangeError * SPEED_GAIN, -MAX_AUTO_SPEED, MAX_AUTO_SPEED);
-                turn   = Range.clip(headingError * TURN_GAIN, -MAX_AUTO_TURN, MAX_AUTO_TURN) ;
-                strafe = Range.clip(-yawError * STRAFE_GAIN, -MAX_AUTO_STRAFE, MAX_AUTO_STRAFE);
+                // Use the speed and turnPower "gains" to calculate how we want the robot to move.
+                forwardPower  = Range.clip(rangeError * forwardGain, -maxAutoForwardSpeed, maxAutoForwardSpeed);
+                turnPower   = Range.clip(headingError * turnGain, -maxAutoTurnSpeed, maxAutoTurnSpeed) ;
+                strafePower = Range.clip(-yawError * strafeGain, -maxAutoStrafeSpeed, maxAutoStrafeSpeed);
 
-                telemetry.addData("Auto","Drive %5.2f, Strafe %5.2f, Turn %5.2f ", drive, strafe, turn);
+                telemetry.addData("Auto","Drive %5.2f, Strafe %5.2f, Turn %5.2f ", forwardPower, strafePower, turnPower);
             } else {
 
-                // drive using manual POV Joystick mode.  Slow things down to make the robot more controllable.
-                drive  = -gamepad1.left_stick_y  / 2.0;  // Reduce drive rate to 50%.
-                strafe = -gamepad1.left_stick_x  / 2.0;  // Reduce strafe rate to 50%.
-                turn   = -gamepad1.right_stick_x / 3.0;  // Reduce turn rate to 33%.
-                telemetry.addData("Manual","Drive %5.2f, Strafe %5.2f, Turn %5.2f ", drive, strafe, turn);
+                // forwardPower using manual POV Joystick mode.  Slow things down to make the robot more controllable.
+                forwardPower  = -gamepad1.left_stick_y  / 2.0;  // Reduce forwardPower rate to 50%.
+                strafePower = -gamepad1.left_stick_x  / 2.0;   // Reduce strafePower rate to 50%.
+                turnPower   = -gamepad1.right_stick_x / 3.0;  // Reduce turnPower rate to 33%.
+                telemetry.addData("Manual","Drive %5.2f, Strafe %5.2f, Turn %5.2f ", forwardPower, strafePower, turnPower);
             }
             telemetry.update();
 
             // Apply desired axes motions to the drivetrain.
-            moveRobot(drive, strafe, turn);
+            moveRobot(forwardPower, strafePower, turnPower);
             sleep(10);
         }
     }
 
-    /**
-     * Move robot according to desired axes motions
-     * <p>
-     * Positive X is forward
-     * <p>
-     * Positive Y is strafe left
-     * <p>
-     * Positive Yaw is counter-clockwise
-     */
     public void moveRobot(double x, double y, double yaw) {
         // Calculate wheel powers.
         double driveFrontLeftPower = x -y -yaw;
@@ -196,9 +172,6 @@ DcMotor driveBackRight;
         driveBackRight.setPower(driveBackRightPower);
     }
 
-    /**
-     * Initialize the AprilTag processor.
-     */
     private void initAprilTag() {
         // Create the AprilTag processor by using a builder.
         aprilTag = new AprilTagProcessor.Builder().build();
@@ -217,10 +190,6 @@ DcMotor driveBackRight;
         }
     }
 
-    /*
-     Manually set the camera gain and exposure.
-     This can only be called AFTER calling initAprilTag(), and only works for Webcams;
-    */
     private void    setManualExposure(int exposureMS, int gain) {
         // Wait for the camera to be open, then use the controls
 
